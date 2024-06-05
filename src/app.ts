81a1042d26d7421533e9a45a6f57da9f7eb840ef
tigermursa <mursalin2311@gmail.com>
2024-06-06 02:43:18 +0600

    fixing cors errors

--- src/app.ts
+++ src/app.ts
@@ -1,40 +1,32 @@
-// Import the 'express' library which is a web application framework for Node.js
-import express from "express";
-
-
-// Import the 'cors' library which allows handling Cross-Origin Resource Sharing
-import cors from "cors";
+import express, { Request, Response } from "express";
+import cors, { CorsOptions } from "cors";
 import { ProductRoutes } from "./app/modules/product/product.route";
 import { UserRoutes } from "./app/modules/user/users.route";
 import { AuthRoutes } from "./app/modules/auth/auth.route";
 
+// Load environment variables from .env file
+require('dotenv').config();
 
-// Create an instance of an Express application
 const app = express();
 
-
-// Middleware to parse JSON bodies from incoming requests
 app.use(express.json());
 
+// Middleware to enable CORS for all incoming requests with credentials
+const corsOptions: CorsOptions = {
+    origin: (origin, callback) => {
+        callback(null, true); // Allow all origins
+    },
+    credentials: true, // Allow credentials (cookies, authorization headers, etc.)
+};
 
-// Middleware to enable CORS for all incoming requests
-app.use(cors());
+app.use(cors(corsOptions));
 
+app.use('/api/v1/product', ProductRoutes); // Products
+app.use('/api/v2/user', UserRoutes); // Users
+app.use('/api/v3/user', AuthRoutes); // Login
 
-// Define application routes
-
-app.use('/api/v1/product', ProductRoutes); //Products
-app.use('/api/v2/user', UserRoutes); //Users
-app.use('/api/v3/user', AuthRoutes);       //Login 
-
-// Define a route for the root URL that sends a greeting message
-app.get('/', (req, res) => {
+app.get('/', (req: Request, res: Response) => {
     res.send('The Briyani Boss Server Running');
 });
 
-
-// Export the Express app instance as the default export
 export default app;
-
-
-//? maintaining double space between lines
